18eb537193f808365462be318a9a220c5075f9d3
Joshua Turner <joshturner747@hotmail.com>
2023-04-09 10:27:27 +1000

    drone client started

---
+++ src/DroneClient.java
@@ -0,0 +1,49 @@
+import Models.Drone;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class DroneClient {
+    public static void main (String args[]) {
+
+        Socket s = null;
+
+        Drone drone = Drone.createDrone();
+
+        // todo: move to method
+        try{
+
+            int serverPort = 8888;
+            s = new Socket("localhost", serverPort);
+
+            ObjectInputStream in = null;
+            ObjectOutputStream out =null;
+            out =new ObjectOutputStream(s.getOutputStream());
+            in = new ObjectInputStream( s.getInputStream());
+
+            out.writeObject(drone);
+
+
+        }catch (UnknownHostException e){
+            System.out.println("Socket:"+e.getMessage());
+        }catch (EOFException e){
+            System.out.println("EOF:"+e.getMessage());
+        }catch (IOException e){
+            System.out.println("readline:"+e.getMessage());
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally {
+            if(s!=null) {
+                try {
+                    s.close();
+                }catch (IOException e){
+                    System.out.println("close:"+e.getMessage());
+                }
+            }
+        }
+    }
+}
---
+++ src/Models/Drone.java
@@ -0,0 +1,103 @@
+package Models;
+
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Drone implements Serializable {
+    private int id;
+    private String name;
+    private int xCoordinate;
+    private int yCoordinate;
+    private Fire fire;
+
+    public Drone() {}
+
+    public Drone(int id, String name, int xCoordinate, int yCoordinate, Fire fire) {
+        this.id = id;
+        this.name = name;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.fire = fire;
+    }
+
+    public Drone(Drone drone) {
+        this.id = drone.id;
+        this.name = drone.name;
+        this.xCoordinate = drone.xCoordinate;
+        this.yCoordinate = drone.yCoordinate;
+        this.fire = drone.fire;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getXCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setXCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getYCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setYCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public Fire getFire() {
+        return fire;
+    }
+
+    public void setFire(Fire fire) {
+        this.fire = fire;
+    }
+
+    public boolean fireSpotted() {
+        return fire != null;
+    }
+
+    @Override
+    public String toString() {
+        return "Models.Drone{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", xCoordinate=" + xCoordinate +
+                ", yCoordinate=" + yCoordinate +
+                ", fire=" + fire +
+                '}';
+    }
+
+    public static Drone createDrone(){
+        Drone d = new Drone();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Drone Creation:");
+
+        System.out.print("Enter drone id: ");
+        d.setId(in.nextInt());
+
+        System.out.print("Enter drone name: ");
+        d.setName(in.nextLine());
+
+        d.setXCoordinate(0);
+        d.setYCoordinate(0);
+
+        return d;
+    }
+}
