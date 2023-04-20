13c972ebeec3a53971c0239fc90214b031272b92
Joshua Turner <joshturner747@hotmail.com>
2023-04-21 02:14:24 +1000

    code close, some bugs

--- src/DroneClient.java
+++ src/DroneClient.java
@@ -1,12 +1,8 @@
 import Controllers.FireLocations;
-import Models.Drone;
-import Models.Fire;
-import Models.ServerResponse;
+import Models.*;
 
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Timer;
@@ -17,16 +13,62 @@ public class DroneClient {
 
         FireLocations fl = FireLocations.getInstance();
         Drone drone = Drone.createDrone();
-        drone.sendUpdate(null, true);
+        Socket s = drone.sendUpdate(null, true, null);
 
         final int MAX_X = 800;
         final int MAX_Y = 500;
+
+        Thread listener = new Thread(() -> {
+            //while(true){
+                try {
+                    ObjectInputStream in = new ObjectInputStream( s.getInputStream() );
+
+                    DroneMessage inMessage = (DroneMessage) in.readObject();
+
+                    Drone inDrone = inMessage.getDrone();
+
+                    if (inMessage.getStatus() == DroneStatus.DELETE) {
+                        System.out.println("Drone shut down from server");
+                        System.exit(0);
+                    }
+
+                    if (inMessage.getStatus() == DroneStatus.UPDATE){
+                        drone.goToLocation(inDrone.getXCoordinate(), inDrone.getYCoordinate());
+                    }
+
+                    //try {
+                        ObjectOutputStream out = new ObjectOutputStream( s.getOutputStream() );
+                        DroneMessage outMessage = new DroneMessage();
+                        outMessage.setStatus(DroneStatus.SUCCESS);
+                        outMessage.setMessage("Drone update successful");
+                        out.writeObject(outMessage);
+//                    } catch (IOException e) {
+//                        throw new RuntimeException(e);
+//                    }
+
+//                    if (inMessage != null){
+//                        Thread messageHandler = new Thread(() -> {
+//
+//                        });
+//                        messageHandler.start();
+//                    }
+
+                } catch (IOException e) {
+                    // Handle any exceptions
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            //}
+
+        });
+        listener.start();
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                drone.sendUpdate(null, false);
+                drone.sendUpdate(null, false, s);
             }
         }, 10000, 10000);
 
@@ -39,11 +81,11 @@ public class DroneClient {
 
                         if(fl.inList(drone.getXCoordinate(), drone.getYCoordinate())){
                             Fire fire = new Fire(fl.getFire(drone.getXCoordinate(), drone.getYCoordinate()));
-                            drone.sendUpdate(fire, false);
+                            drone.sendUpdate(fire, false, s);
                         }
 
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(10);
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
@@ -57,11 +99,11 @@ public class DroneClient {
 
                         if(fl.inList(drone.getXCoordinate(), drone.getYCoordinate())){
                             Fire fire = new Fire(fl.getFire(drone.getXCoordinate(), drone.getYCoordinate()));
-                            drone.sendUpdate(fire, false);
+                            drone.sendUpdate(fire, false, s);
                         }
 
                         try {
-                            Thread.sleep(100);
+                            Thread.sleep(10);
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
